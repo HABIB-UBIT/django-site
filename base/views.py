471d5dd53c090aa6e532ce94bf6fd3619f87abd5
Muhammad Habib <habibsahabubit@gmail.com>
2024-02-08 03:31:52 +0500

    theme update

--- base/views.py
+++ base/views.py
@@ -62,24 +62,36 @@ def home(request):
         Q(topic__name__icontains=q)  |
         Q(name__icontains=q) |
         Q(description__icontains=q)
-    )    ### this is what we call query set  query_set= ModelName.objects.method()
+    )    
+    ### this is what we call query set  query_set= ModelName.objects.method()
     topics= Topics.objects.all()
     room_count= rooms.count()
-    context= {'rooms':rooms, 'topics':topics, 'room_count': room_count}
+    room_messages= Message.objects.filter(room__topic__name__icontains=q)
+    context= {'rooms':rooms, 'topics':topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context )
 
 def room(request,pk):
     room= Room.objects.get(id=pk)
-    room_messages= room.message_set.all()     ##### we are asking for all the messages of that specific room   ### We are intrested in getting all the messages of that specific room
+    room_messages= room.message_set.all()  ##_set.all() mostly use eith many one to relationships   ##### we are asking for all the messages of that specific room   ### We are intrested in getting all the messages of that specific room
+    participants= room.participants.all()   ## .all() used with many to many relationships
     if request.method == 'POST':
         message = Message.objects.create(
             user=request.user,
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)  ## If the person writes the comment he will be directly become a participant
         return redirect('room', pk=room.id)
-    context={'room':room, 'room_messages':room_messages}
+    context={'room':room, 'room_messages':room_messages, 'participants': participants}
     return render(request, 'base/room.html',context)
+
+def userProfile(request,pk):
+    user= User.objects.get(id=pk)
+    rooms= user.room_set.all()
+    room_messages= user.message_set.all()
+    topics= Topics.objects.all()
+    context={'user':user, 'rooms':rooms, 'room_messages': room_messages, 'topics': topics}
+    return render(request, 'base/profile.html', context)
 
 ######### CREATE FUNCTION ###########
 @login_required(login_url='login')
@@ -88,7 +100,9 @@ def createroom(request):
     if request.method == 'POST':
         form= RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room= form.save(commit=False)
+            room.host= request.user
+            room.save()
             return redirect ('home')
         
     context={'form':form}
@@ -115,9 +129,20 @@ def updateroom(request,pk):
 def deleteroom(request,pk):
     room= Room.objects.get(id=pk)
     if request.user != room.host:
-        return HttpResponse('You are not allowed to edit here')
+        return HttpResponse('You are not allowed to delete')
     if request.method=='POST':
         room.delete()
         return redirect ('home')
     return render(request, 'base/delete.html', {'obj': room})
 
+
+######### DELETE Message ###########
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse('You are not allowed to delete')
+    if request.method=='POST':
+        message.delete()
+        return redirect ('home')
+    return render(request, 'base/delete.html', {'obj': message})
